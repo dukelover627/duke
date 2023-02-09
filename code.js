47c434c8aafe26a8d7bc99bd40fdf065e58286b3
dukelover627 <122392812+dukelover627@users.noreply.github.com>
2023-02-09 11:00:22 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,99 +1,41 @@
 // fuction that runs timer
 function runTimer(countdown){
+
     // this variable the timer with the countdown that shows on the page
-var currtime=50;
+var currTime=50;
+
 // timeout tracks delay 
 var timeout=0;
+
 // determins how long the delay
 var timeoutincrement=5000;
+
+for(var counter=0; counter < 10; counter++){
 // built in fucntion run on delay
 setTimeout(function(){
-// where the contdown display
-    countdown.innerHTML=currtime;
-    // we -5 so timer can go to zero
-    currtime=currtime-5;
-
-},timeout);
-timeout=timeout+timeoutincrement;
-
-setTimeout(function(){
-
-    countdown.innerHTML=currtime;
-    currtime=currtime-5;
-
-},timeout);
-timeout=timeout+timeoutincrement;
-
-setTimeout(function(){
-
-    countdown.innerHTML=currtime;
-    currtime=currtime-5;
-
-},timeout);
-timeout=timeout+timeoutincrement;
-
-setTimeout(function(){
-
-    countdown.innerHTML=currtime;
-    currtime=currtime-5;
-
-},timeout);
-timeout=timeout+timeoutincrement;
-
-setTimeout(function(){
-
-    countdown.innerHTML=currtime;
-    currtime=currtime-5;
-
-},timeout);
-timeout=timeout+timeoutincrement;
-
-setTimeout(function(){
-
-    countdown.innerHTML=currtime;
-    currtime=currtime-5;
-
-},timeout);
-timeout=timeout+timeoutincrement;
-
-setTimeout(function(){
-
-    countdown.innerHTML=currtime;
-    currtime=currtime-5;
-
-},timeout);
-timeout=timeout+timeoutincrement;
-
-setTimeout(function(){
-
-    countdown.innerHTML=currtime;
-    currtime=currtime-5;
-
-},timeout);
-timeout=timeout+timeoutincrement;
-
-setTimeout(function(){
-
-    countdown.innerHTML=currtime;
-    currtime=currtime-5;
-
-},timeout);
-timeout=timeout+timeoutincrement;
-
-setTimeout(function(){
-
-    countdown.innerHTML=currtime;
-    currtime=currtime-5;
-
-},timeout);
-timeout=timeout+timeoutincrement;
-//last timout with alert
-setTimeout(function(){
-    countdown.innerHTML=currtime;
+    if(currTime == 0){
     //alert to balst off
     alert("blast off");
+    countdowneElem.innerHTML = "launch successful!";
+}
+else if (currTime <25){
+    countdown.innerHTML= "Warning Less than 1/2 way to launch, time left = insert variable here = " + currTime;
+}
+else{
+    countdown.innerHTML=currTime;
+}
+
+// where the contdown display
+    countdown.innerHTML=currTime;
+
+    // we -5 so timer can go to zero
+    currtime=currTime-5;
 
 },timeout);
+timeout=timeout+timeoutincrement;
+}
+
+
 }
 //empty for now
 function startbuttonclick(){}
