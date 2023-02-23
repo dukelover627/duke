fb4f8bf6fe47b558618969c73c2ef83b3bd8c47e
dukelover627 <122392812+dukelover627@users.noreply.github.com>
2023-02-23 11:29:22 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -10,9 +10,9 @@ var timeout=0;
 // determins how long the delay
 var timeoutincrement=5000;
 
-for(var counter=0; counter < 10; counter++){
+for(var counter=0; counter < 11; counter++){
 // built in fucntion run on delay
-setTimeout(function(){
+arrInterval[counter] = setTimeout(function(){
     if(currTime == 0){
     //alert to balst off
     alert("blast off");
@@ -26,10 +26,10 @@ else{
 }
 
 // where the contdown display
-    countdown.innerHTML=currTime;
+
 
     // we -5 so timer can go to zero
-    currtime=currTime-5;
+    currTime=currTime-5;
 
 },timeout);
 timeout=timeout+timeoutincrement;
@@ -37,6 +37,23 @@ timeout=timeout+timeoutincrement;
 
 
 }
+var arrInterval = new Array();
 //empty for now
-function startbuttonclick(){}
-function stopbuttonclick(){}
+function startbuttonclick(){
+    //disable start button once clicked
+    document.getElementById("btnStart").disabled = true;
+    //disables stop button once clicked
+    document.getElementById("btnStop").disabled = false;
+            //pionter to html span that shows countdown
+var countdown=document.getElementById("countdown");
+// calls javascript to make funtion 
+runTimer(countdown);
+}
+function stopbuttonclick(){
+    document.getElementById("btnStart").disabled = false;
+    document.getElementById("btnStop").disabled = true;
+    //clears all delays code
+    for (counter = 0; counter < 11; counter++){
+        clearTimeout(arrInterval[counter]);
+    }
+}
